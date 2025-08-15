e57873afb0e9a8d89124e332f795302a73e861ab
Abdiwaahid <abdiwaahid01@gmail.com>
2025-08-15 08:57:40 +0200

    upgrade to tailwindcss4

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -53,7 +53,6 @@ class AdminPanelProvider extends PanelProvider
                 SubstituteBindings::class,
                 DisableBladeIconComponents::class,
                 DispatchServingFilamentEvent::class,
-                \Hasnayeen\Themes\Http\Middleware\SetTheme::class
             ])
             ->authMiddleware([
                 Authenticate::class,
